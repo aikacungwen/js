99469f559d5f2211f9bf6fafbcfe59927f5352fc
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-19 13:45:00 +0700

    Create sl2.js

---
+++ sl2.js
@@ -0,0 +1,24 @@
+// Jumlah episode
+    var totalEpisodes = 35;
+
+    // Tangkap elemen tabs
+    var tabsContainer = document.querySelector('.tabs');
+
+    // Mendapatkan URL halaman saat ini
+    var currentPageUrl = window.location.href;
+
+    // Buat tab-episode secara dinamis
+    for (var i = 35; i <= totalEpisodes; i++) {
+        var episodeUrl = 'https://iqiyoo.blogspot.com/p/rene-' + i + '.html';
+
+        var tab = document.createElement('a');
+        tab.setAttribute('href', episodeUrl);
+        tab.textContent = ' ' + i;
+
+        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+        if (currentPageUrl.includes('/rene-' + i + '.html')) {
+            tab.classList.add('active');
+        }
+
+        tabsContainer.appendChild(tab);
+    }
