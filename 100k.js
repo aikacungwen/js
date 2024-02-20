a666285e71239ab6a3856d2bf8c87c6fdaf3899d
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-21 01:15:03 +0700

    Create 100k.js

---
+++ 100k.js
@@ -0,0 +1,24 @@
+// Jumlah episode
+    var totalEpisodes = 109;
+
+    // Tangkap elemen tabs
+    var tabsContainer = document.querySelector('.tabs');
+
+    // Mendapatkan URL halaman saat ini
+    var currentPageUrl = window.location.href;
+
+    // Buat tab-episode secara dinamis
+    for (var i = 109; i <= totalEpisodes; i++) {
+        var episodeUrl = 'https://1ac-site.blogspot.com/p/100k-' + i + '.html';
+
+        var tab = document.createElement('a');
+        tab.setAttribute('href', episodeUrl);
+        tab.textContent = ' ' + i;
+
+        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+        if (currentPageUrl.includes('/100k-' + i + '.html')) {
+            tab.classList.add('active');
+        }
+
+        tabsContainer.appendChild(tab);
+    }
