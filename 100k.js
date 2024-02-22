dea166745056ce97ea0c0f4a0c3dbdb9029ef737
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 05:22:35 +0700

    Update 100k.js

--- 100k.js
+++ 100k.js
@@ -9,7 +9,7 @@
 
     // Buat tab-episode secara dinamis
     for (var i = 109; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://1ac-site.blogspot.com/p/100k-' + i + '.html';
+        var episodeUrl = 'https://play.1ac.site/p/100k-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
