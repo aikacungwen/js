c5aa8e139894843cb570f83ffda16083e249ed7b
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 06:40:27 +0700

    Create mele.js

---
+++ mele.js
@@ -0,0 +1,24 @@
+// Jumlah episode
+    var totalEpisodes = 19;
+
+    // Tangkap elemen tabs
+    var tabsContainer = document.querySelector('.tabs');
+
+    // Mendapatkan URL halaman saat ini
+    var currentPageUrl = window.location.href;
+
+    // Buat tab-episode secara dinamis
+    for (var i = 19; i <= totalEpisodes; i++) {
+        var episodeUrl = 'https://play.1ac.site/p/mele-' + i + '.html';
+
+        var tab = document.createElement('a');
+        tab.setAttribute('href', episodeUrl);
+        tab.textContent = ' ' + i;
+
+        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+        if (currentPageUrl.includes('/mele-' + i + '.html')) {
+            tab.classList.add('active');
+        }
+
+        tabsContainer.appendChild(tab);
+    }
