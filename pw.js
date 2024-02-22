3d0cfdeffc387baf326e52b3c096cd57b81a7ab2
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 05:24:15 +0700

    Update pw.js

--- pw.js
+++ pw.js
@@ -9,7 +9,7 @@
 
     // Buat tab-episode secara dinamis
     for (var i = 151; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://1ac-site.blogspot.com/p/pw-' + i + '.html';
+        var episodeUrl = 'https://play.1ac.site/p/pw-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
