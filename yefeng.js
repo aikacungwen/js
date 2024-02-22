89941bea7b3d2b7ef5b646991a57f1377f61404c
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 05:26:05 +0700

    Update yefeng.js

--- yefeng.js
+++ yefeng.js
@@ -9,7 +9,7 @@
 
     // Buat tab-episode secara dinamis
     for (var i = 44; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://1ac-site.blogspot.com/p/yefeng-' + i + '.html';
+        var episodeUrl = 'https://play.1ac.site/p/yefeng-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
