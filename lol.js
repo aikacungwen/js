a37c50d358ae2037350c19f39631734e47fed651
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 05:23:14 +0700

    Update lol.js

--- lol.js
+++ lol.js
@@ -9,7 +9,7 @@
 
     // Buat tab-episode secara dinamis
     for (var i = 1; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://1ac-site.blogspot.com/p/lol-' + i + '.html';
+        var episodeUrl = 'https://play.1ac.site/p/lol-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
