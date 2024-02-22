a18467c195fff0e2ea7a7f74c5673c38cf4c5872
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 05:23:41 +0700

    Update moon.js

--- moon.js
+++ moon.js
@@ -9,7 +9,7 @@
 
     // Buat tab-episode secara dinamis
     for (var i = 11; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://1ac-site.blogspot.com/p/moon-' + i + '.html';
+        var episodeUrl = 'https://play.1ac.site/p/moon-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
