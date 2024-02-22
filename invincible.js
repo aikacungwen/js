f65c1783f2c66deac1ad4a7999fb856fdfe37c00
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 06:42:12 +0700

    Update invincible.js

--- invincible.js
+++ invincible.js
@@ -1,1 +1,24 @@
+// Jumlah episode
+    var totalEpisodes = 17;
 
+    // Tangkap elemen tabs
+    var tabsContainer = document.querySelector('.tabs');
+
+    // Mendapatkan URL halaman saat ini
+    var currentPageUrl = window.location.href;
+
+    // Buat tab-episode secara dinamis
+    for (var i = 17; i <= totalEpisodes; i++) {
+        var episodeUrl = 'https://play.1ac.site/p/invincible-' + i + '.html';
+
+        var tab = document.createElement('a');
+        tab.setAttribute('href', episodeUrl);
+        tab.textContent = ' ' + i;
+
+        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+        if (currentPageUrl.includes('/invincible-' + i + '.html')) {
+            tab.classList.add('active');
+        }
+
+        tabsContainer.appendChild(tab);
+    }
