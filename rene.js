f2fbbc96459b2abb6cc675d04fe909dd2431d8c4
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 05:24:36 +0700

    Update rene.js

--- rene.js
+++ rene.js
@@ -9,7 +9,7 @@
 
     // Buat tab-episode secara dinamis
     for (var i = 24; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://1ac-site.blogspot.com/p/rene-' + i + '.html';
+        var episodeUrl = 'https://play.1ac.site/p/rene-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
