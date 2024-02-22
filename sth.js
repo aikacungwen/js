abe8cdd02ba1b8639efa36e7220cb30ad5f9c706
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 05:25:44 +0700

    Update sth.js

--- sth.js
+++ sth.js
@@ -9,7 +9,7 @@
 
     // Buat tab-episode secara dinamis
     for (var i = 45; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://1ac-site.blogspot.com/p/sth-' + i + '.html';
+        var episodeUrl = 'https://play.1ac.site/p/sth-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
