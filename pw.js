e22b7a4ca89abf487b226abaf4974b0a56bafd6e
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-02-23 04:08:12 +0700

    Create pw.js

---
+++ pw.js
@@ -0,0 +1,24 @@
+// Jumlah episode
+    var totalEpisodes = 151;
+
+    // Tangkap elemen tabs
+    var tabsContainer = document.querySelector('.tabs');
+
+    // Mendapatkan URL halaman saat ini
+    var currentPageUrl = window.location.href;
+
+    // Buat tab-episode secara dinamis
+    for (var i = 151; i <= totalEpisodes; i++) {
+        var episodeUrl = 'https://1ac-site.blogspot.com/p/pw-' + i + '.html';
+
+        var tab = document.createElement('a');
+        tab.setAttribute('href', episodeUrl);
+        tab.textContent = ' ' + i;
+
+        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+        if (currentPageUrl.includes('/pw-' + i + '.html')) {
+            tab.classList.add('active');
+        }
+
+        tabsContainer.appendChild(tab);
+    } 
