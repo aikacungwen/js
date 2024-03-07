9f2bfffbac0f317ebe2c610abee376b87ee26573
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-03-07 11:17:55 +0700

    Update big2.js

--- big2.js
+++ big2.js
@@ -1,24 +1,16 @@
-// Jumlah episode
-    var totalEpisodes = 14;
+var totalEpisodes = 15; // Ubah total episode menjadi 15
 
-    // Tangkap elemen tabs
-    var tabsContainer = document.querySelector('.tabs');
+for (var i = 13; i <= totalEpisodes; i++) {
+    var episodeUrl = i === 14 ? 'https://play.1ac.site/p/big2-14_6.html' : 'https://play.1ac.site/p/big2-' + i + '.html';
 
-    // Mendapatkan URL halaman saat ini
-    var currentPageUrl = window.location.href;
+    var tab = document.createElement('a');
+    tab.setAttribute('href', episodeUrl);
+    tab.textContent = ' ' + i;
 
-    // Buat tab-episode secara dinamis
-    for (var i = 12; i <= totalEpisodes; i++) {
-        var episodeUrl = 'https://play.1ac.site/p/big2-' + i + '.html';
-
-        var tab = document.createElement('a');
-        tab.setAttribute('href', episodeUrl);
-        tab.textContent = ' ' + i;
-
-        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
-        if (currentPageUrl.includes('/big2-' + i + '.html')) {
-            tab.classList.add('active');
-        }
-
-        tabsContainer.appendChild(tab);
+    // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+    if (currentPageUrl.includes('/big2-' + i + '.html')) {
+        tab.classList.add('active');
     }
+
+    tabsContainer.appendChild(tab);
+}
