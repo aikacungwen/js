1a641fddbc77a85c55b81ff7c2c28c33634ca80a
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-03-09 14:49:07 +0700

    Update money2.js

--- money2.js
+++ money2.js
@@ -8,21 +8,31 @@ var tabsContainer = document.querySelector('.tabs');
 var currentPageUrl = window.location.href;
 
 // Buat tab-episode secara dinamis
-for (var i = 61; i <= totalEpisodes; i++) {
+var mergedEpisodeUrl = 'https://play.1ac.site/p/money2-6164.html'; // Ganti "100k" dengan "money2"
+
+var mergedTab = document.createElement('a');
+mergedTab.setAttribute('href', mergedEpisodeUrl);
+mergedTab.textContent = 'Episodes [61-64]';
+
+// Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+if (currentPageUrl.includes('/money2-6164.html')) { // Ganti "/100k-" dengan "/money2-"
+    mergedTab.classList.add('active');
+}
+
+// Tambahkan tab gabungan ke dalam container
+tabsContainer.appendChild(mergedTab);
+
+// Buat tab-episode untuk episode 65 dan seterusnya
+for (var i = 65; i <= totalEpisodes; i++) {
     var episodeUrl = 'https://play.1ac.site/p/money2-' + i + '.html'; // Ganti "100k" dengan "money2"
 
     var tab = document.createElement('a');
     tab.setAttribute('href', episodeUrl);
-    
-    if (i === 61) {
-        tab.textContent = '61-64';
-        tab.classList.add('active'); // Set kelas active untuk episode [61-64]
-    } else {
-        tab.textContent = ' ' + i;
-        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
-        if (currentPageUrl.includes('/money2-' + i + '.html')) { // Ganti "/100k-" dengan "/money2-"
-            tab.classList.add('active');
-        }
+    tab.textContent = ' ' + i;
+
+    // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+    if (currentPageUrl.includes('/money2-' + i + '.html')) { // Ganti "/100k-" dengan "/money2-"
+        tab.classList.add('active');
     }
 
     tabsContainer.appendChild(tab);
