bf920982bc7ddc089c7b95146d2b60a6fbc8ef1c
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-03-23 00:56:00 +0700

    Create test.js

---
+++ manual/test.js
@@ -0,0 +1,31 @@
+// Tangkap elemen tabs
+var tabsContainer = document.querySelector('.tabs');
+
+// Array yang berisi link untuk setiap episode
+var episodeLinks = [
+    'https://play.1ac.site/p/al-1.html',
+    'https://play.1ac.site/p/al-2.html',
+    'https://play.1ac.site/p/al-3.html',
+    'https://play.1ac.site/p/al-4.html',
+    'https://play.1ac.site/p/al-5.html',
+    'https://play.1ac.site/p/al-6.html'
+];
+
+// Mendapatkan URL halaman saat ini
+var currentPageUrl = window.location.href;
+
+// Buat tab-episode secara dinamis
+for (var i = 1; i <= episodeLinks.length; i++) {
+    var episodeUrl = episodeLinks[i - 1];
+
+    var tab = document.createElement('a');
+    tab.setAttribute('href', episodeUrl);
+    tab.textContent = ' ' + i;
+
+    // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+    if (currentPageUrl === episodeUrl) {
+        tab.classList.add('active');
+    }
+
+    tabsContainer.appendChild(tab);
+}
