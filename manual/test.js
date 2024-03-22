f85ae2827733dce363f7e0cd401a6bb14753e684
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-03-23 01:11:41 +0700

    Update test.js

--- manual/test.js
+++ manual/test.js
@@ -1,14 +1,13 @@
 // Tangkap elemen tabs
 var tabsContainer = document.querySelector('.tabs');
 
-// Array yang berisi link untuk setiap episode
 var episodeLinks = [
     'https://play.1ac.site/p/al-1.html',
     'https://play.1ac.site/p/al-2.html',
     'https://play.1ac.site/p/al-3.html',
     'https://play.1ac.site/p/al-4.html',
     'https://play.1ac.site/p/al-5.html',
-    'https://play.1ac.site/p/al-6.html'
+    'https://play.1ac.site/p/al-6.html',
     'https://play.1ac.site/p/al-7.html',
     'https://play.1ac.site/p/al-8.html',
     'https://play.1ac.site/p/al-9.html',
@@ -16,6 +15,7 @@ var episodeLinks = [
     'https://play.1ac.site/p/al-11.html',
     'https://play.1ac.site/p/al-12.html'
 ];
+
 
 // Mendapatkan URL halaman saat ini
 var currentPageUrl = window.location.href;
