c87ff059c9350d88111182604eef449af16fde04
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-03-23 01:06:35 +0700

    Update test.js

--- manual/test.js
+++ manual/test.js
@@ -9,6 +9,12 @@ var episodeLinks = [
     'https://play.1ac.site/p/al-4.html',
     'https://play.1ac.site/p/al-5.html',
     'https://play.1ac.site/p/al-6.html'
+    'https://play.1ac.site/p/al-7.html',
+    'https://play.1ac.site/p/al-8.html',
+    'https://play.1ac.site/p/al-9.html',
+    'https://play.1ac.site/p/al-10.html',
+    'https://play.1ac.site/p/al-11.html',
+    'https://play.1ac.site/p/al-12.html'
 ];
 
 // Mendapatkan URL halaman saat ini
