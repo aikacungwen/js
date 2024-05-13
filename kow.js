68c549f68330360fa311ab8377eaa7886a9996f5
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-05-13 11:21:26 +0700

    Update kow.js

--- kow.js
+++ kow.js
@@ -1,5 +1,5 @@
-// Jumlah episode
-var totalEpisodes = 9;
+Y// Jumlah episode
+var totalEpisodes = 10;
 
 // Tangkap elemen kontainer tab
 var tabsContainer = document.querySelector('.tab-container .tabs tbody');
