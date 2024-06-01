20de56f2988d98c85b9f6827e109d91d8884e37c
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-06-01 14:09:38 +0700

    Create siliang.js

---
+++ siliang.js
@@ -0,0 +1,24 @@
+// Jumlah episode
+    var totalEpisodes = 1;
+
+    // Tangkap elemen tabs
+    var tabsContainer = document.querySelector('.tabs');
+
+    // Mendapatkan URL halaman saat ini
+    var currentPageUrl = window.location.href;
+
+    // Buat tab-episode secara dinamis
+    for (var i = 1; i <= totalEpisodes; i++) {
+        var episodeUrl = 'https://play.1ac.site/p/siliang-' + i + '.html';
+
+        var tab = document.createElement('a');
+        tab.setAttribute('href', episodeUrl);
+        tab.textContent = ' ' + i;
+
+        // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
+        if (currentPageUrl.includes('/siliang-' + i + '.html')) {
+            tab.classList.add('active');
+        }
+
+        tabsContainer.appendChild(tab);
+    } 
