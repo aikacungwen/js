213dbf29b9f297f0098f93127040257d10b39910
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-09-19 11:06:14 +0700

    Update big2.js

--- big2.js
+++ big2.js
@@ -1,4 +1,4 @@
-var totalEpisodes = 41; // Ubah total episode menjadi 15
+var totalEpisodes = 42; // Ubah total episode menjadi 15
 
 // Tangkap elemen tabs
 var tabsContainer = document.querySelector('.tabs');
